1a9a073c82838a1bc6cab8fcf9d06fe7a6106d62
Victor Caballero <vikcaballero@outlook.com>
2026-06-23 23:21:24 -0600

    SafeAreaView & ScrollView

--- Practica5/my-app2/screens/ImageBackgroundScreen.js
+++ Practica5/my-app2/screens/ImageBackgroundScreen.js
@@ -1,25 +1,53 @@
-/* Zona1: Importaciones de componentes y archivos */
+/* Zona1: Importaciones de componenetes y archivos */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Button } from 'react-native';
+import { useState } from 'react';
+
 
 /* Zona2: Main - Hogar de los componentes */
 export default function ImageBackgroundScreen() {
-    return (
-        <View style={styles.container}>
-            <Text>Aquí va la práctica de ImageBackground</Text>
 
-            <StatusBar style="auto" />
-        </View>
+  const [fondo, setFondo] = useState(
+        require('../assets/fondo.jpg')
+        );
+  
+  const cambiarFondo = () => {
+    setFondo (
+      fondo === require('../assets/fondo.jpg')
+      ? require('../assets/fondo2.jpg')
+      : require('../assets/fondo.jpg')
     );
+  }
+    
+  return (
+     <ImageBackground
+            source={fondo}
+            style={styles.fondo}
+            resizeMode="cover"
+        >
+            <View style={styles.centro}>
+                <Text style={styles.texto}>Bienvenido</Text>
+                <Button title="Cambiar Fondo" onPress={cambiarFondo} />
+            </View>
+            <StatusBar style="auto" />
+      </ImageBackground>
+  );
 }
 
 /* Zona3: Estilos y Posicionamiento */
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-  },
+   fondo: {
+        flex: 1,
+        width: '100%',
+    },
+    centro: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    texto: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        color: 'white',
+    },
 });
